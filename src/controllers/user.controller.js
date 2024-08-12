864a36a63fe69696d02dbb80fc6aae50295f6616
Ajay Kumar <ajayasrdeveloper1@gmail.com>
2024-08-12 15:53:29 +0530

    update invoice and push

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import InvoiceDB from "../models/Invoice.model.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import csv from "csvtojson"
 import hbs from "handlebars";
-const file1 = "./public/filefirst1.csv";
+const file1 = "./public/testt.csv";
 import puppeteer from 'puppeteer';
 import fs from "fs-extra"
 import path from "path"
@@ -20,24 +20,20 @@ export const storeData = asyncHandler(async (req, res) => {
 })
 
 export const generatePdf = asyncHandler(async (req, res) => {
-    let data = { Title: "hello this is title", Information: { amount: 500, description: "dfd", BillMobileNumber: "fdf", BillTo: "hell", GSTNumber: "helo", BillingCompanyAdd: "ddf", BillingCompanyName: "df", RefId: "ff", InvoiceDate: "rtr", RRN: "rtr", InvoiceNumber: "4354f" } };
 
     try {
         const browser = await puppeteer.launch({ headless: true });
-        // const page = await browser.newPage();
-        // let content = await compileTemplete("index", data)
-        // // set the content in pdf 
-        // await page.setContent(content);
-        // await page.emulateMedia('screen');
 
-        for (let i = 0; i < 100; i++) {
+        const db = await InvoiceDB.find();
+
+        for (let i = 0; i < db.length; i++) {
+            let data = { RRN: db[i].RRN, MerchantName: db[i].MerchantName, Txndate: db[i].Txndate, RefId: db[i].RefId, Txnamount: db[i].Txnamount, MobileNumber: db[i].MobileNumber };
             const page = await browser.newPage();
             let content = await compileTemplete("index", data)
             await page.setContent(content);
-            // await page.emulateMedia('screen');
             // genrate pdf
             await page.pdf({
-                path: `AllGeneratedPdf/${i}.pdf`,
+                path: `AllGeneratedPdf/${i + 1}.pdf`,
                 format: "A4",
                 printBackgound: true
             })
@@ -45,7 +41,7 @@ export const generatePdf = asyncHandler(async (req, res) => {
         await browser.close();
         // process.exit();
 
-        res.status(200).json({ message: "Success", data: "Generated Successfully" })
+        res.status(200).json({ message: "Success", data: "Generated Successfully", db })
     } catch (error) {
         res.status(400).json({ message: "Faild", data: error })
     }
--- src/models/Invoice.model.js
+++ src/models/Invoice.model.js
@@ -3,21 +3,20 @@ import { Schema, model } from "mongoose";
 const invoiceSchema = new Schema({
     RRN: {
         type: String,
-        required: [true, "Please Enter RRN !"]
     },
     MerchantName: {
         type: String,
-        required: [true, "Please Enter Merchant Id !"]
     },
     Txndate: {
         type: String,
-        required: [true, "Please Select member Type !"]
     },
-    Txntime: {
+    RefId: {
         type: String,
-        required: [true, "Please Select member Type !"]
     },
     Txnamount: {
+        type: String,
+    },
+    MobileNumber: {
         type: String,
     },
     STATUS: {
